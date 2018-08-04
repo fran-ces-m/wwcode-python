02936b439ad970d934ef752a51ef8c3d21f2e215
francesmjcx <42084057+francesmjcx@users.noreply.github.com>
2018-08-04 13:55:14 +0800

    Add files via upload

---
+++ challenge.py
@@ -0,0 +1,7 @@
+total_bill = float(input('How much is your total bill? '))
+payment = float(input('How much is your payment? '))
+change = payment-total_bill
+
+# print('Hi! Your change is {}'.format(change))
+# or
+print(f'Hi! Your change is {change}')
